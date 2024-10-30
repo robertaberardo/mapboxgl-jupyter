c28f5b8a1bf3b6eee027778da168e42d1dc48eab
Roberta Berardo <rberardo33@gmail.com>
2024-10-30 16:57:08 -0300

    Update version

--- mapboxgl/__init__.py
+++ mapboxgl/__init__.py
@@ -1,4 +1,4 @@
 from .viz import CircleViz, GraduatedCircleViz, HeatmapViz, ClusteredCircleViz, ImageViz, RasterTilesViz, ChoroplethViz, LinestringViz, GenericViz
 
-__version__ = "0.10.2"
+__version__ = "0.10.2+place.0.1"
 __all__ = ['CircleViz', 'GraduatedCircleViz', 'HeatmapViz', 'ClusteredCircleViz', 'ImageViz', 'RasterTilesViz', 'ChoroplethViz', 'LinestringViz', 'GenericViz']
